0754fca039b0975c68295bdae499c933bda38251
ppeb <ppeb@ppeb.me>
2026-07-13 21:09:53 -0500

    Serialize motd when querying from server
    This isn't strictly necessary but will reduce the chance of a
    desynchronization of the bot and server's motd state.

--- commands/motd.ts
+++ commands/motd.ts
@@ -43,8 +43,11 @@ export default {
         else {
             const [success, res] = await makeRequest("/api/motd", "GET");
 
-            if (success)
+            if (success) {
                 await interaction.reply({ content: `Current message of the day is:\n${res.Motd}` });
+                state.motd = res.Motd;
+                state.save();
+            }
             else
                 await interaction.reply({ content: `Failed to fetch current message of the day, error: ${res.Error ?? "no error message provided"}` });
         }
